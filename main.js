686145f40f50236154ba093d06504b4411baacaf
Serhii <norman_foley@ukr.net>
2022-11-16 17:17:20 +0200

    add math operators

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ let button = document.querySelectorAll(".button")
 let numBtn = document.querySelectorAll("#numberButton")
 let output = document.querySelector(".result_screen p")
 let clearButton = document.querySelector("#clearButton")
+let sqrtButton = document.querySelector("#sqrtButton")
+let expButton = document.querySelector("#expButton")
+let sumButton = document.querySelector("#sumButton")
+let multButton = document.querySelector("#multButton")
+let divButton = document.querySelector("#divButton")
 
 //Лампа активности
 calcZone.addEventListener("mouseover", function(){greenLight.className="light_on"})
@@ -22,18 +27,100 @@ button.forEach(btn => {
 let firstNum = '';
 let secondNum = '';
 let sign = '';
+
+//сложение
+sumButton.addEventListener("click", function(){
+    sign = sumButton.textContent
+})
+
+//вычитание
+subButton.addEventListener("click", function(){
+    sign = subButton.textContent
+})
+
+//умножение
+multButton.addEventListener("click", function(){
+    sign = multButton.textContent
+})
+
+//деление
+divButton.addEventListener("click", function(){
+    sign = divButton.textContent
+})
+
 numBtn.forEach(numBtn => {
-    numBtn.addEventListener("click", function(getNum) {
+    numBtn.addEventListener("click", function (getNum) {
         let num = getNum.target.textContent
-        firstNum += num 
-        console.log(firstNum)
-        output.textContent = firstNum
+        if (sign == '' && secondNum == '') {
+            firstNum += num
+            output.textContent = firstNum
+        }
+
+        else if (firstNum !== '' && sign !== '') {
+            output.textContent = ''
+            secondNum += num
+            output.textContent = secondNum
+            console.log(secondNum)
+        }
     })
 })
 
+equalButton.addEventListener("click", result)
+
+function result () {    
+    switch (sign) {
+        case "+":
+            res = Number(firstNum) + Number(secondNum);
+            output.textContent = res;
+            break;
+        case "-":
+            res = Number(firstNum) - Number(secondNum);
+            output.textContent = res;  
+            break;
+        case "x":
+            res = Number(firstNum) * Number(secondNum);
+            output.textContent = res; 
+            break;
+        case "/":
+            res = Number(firstNum) / Number(secondNum);
+            output.textContent = res;
+            break;
+    }
+    firstNum = '';
+    secondNum = '';
+    sign = '';
+}
+
+//кнопка сброса(очистки)
 clearButton.addEventListener("click", function(){
     firstNum = '';
     secondNum = '';
     sign = '';
     output.textContent = 0
+})
+
+//Вычисление квадратного корня
+sqrtButton.addEventListener("click", function(){
+    if(firstNum !== '' && secondNum == '') {
+        output.textContent = Math.sqrt(firstNum)
+    }
+    else if (firstNum !== '' || secondNum !== '') { 
+        output.textContent = Math.sqrt(secondNum)
+    }
+    else if (res !== '') { 
+        output.textContent = Math.sqrt(res)
+    }
+})
+
+//Возведение в квадрат
+expButton.addEventListener("click", function(){
+    if(firstNum !== '' && secondNum == '') {
+        output.textContent = firstNum ** 2
+    }
+    else if (firstNum !== '' || secondNum !== '') {
+        output.textContent = secondNum ** 2
+    }
+    else if (res !== '') {
+        output.textContent = res ** 2
+    }
 })
